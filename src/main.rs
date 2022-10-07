a4baa6b21dfda4dc1d24a3bc840744e62c8a3ddd
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-10-07 12:12:05 +0100

    Actually works now?

--- src/main.rs
+++ src/main.rs
@@ -26,33 +26,33 @@ Find the thirteen adjacent digits in the 1000-digit number that have the greates
 use std::ops::Range;
 
 fn main() {
-    let size_of_slice: i32 = 3;
+    let size_of_slice: i128 = 13;
     let number: String = String::from("7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450");
 
-    let mut digits: Vec<i32> = Vec::new();
+    let mut digits: Vec<i128> = Vec::new();
     
     for digit in number.chars() {
-        digits.push(digit.to_digit(10).expect("That is not a valid number.") as i32);
+        digits.push(digit.to_digit(10).expect("That is not a valid number.") as i128);
     }
 
     let mut running: bool = true;
-    let mut largest_product: i32 = 0;
+    let mut largest_product: i128 = 0;
 
     while running {
-        let mut i: i32 = 0;
-        let mut product: i32 = 1;
+        let mut i: i128 = 0;
+        let mut product: i128 = 1;
         for _digit in digits.iter() {
             product = 1;
             if i + size_of_slice > digits.len().try_into().unwrap() {
                 running = false;
                 break;
             }
-            // let slice: Range<i32> = digits[i as usize]..digits[(i + size_of_slice) as usize];
+            // let slice: Range<i128> = digits[i as usize]..digits[(i + size_of_slice) as usize];
 
-            let mut slice: Vec<i32> = Vec::new();
+            let mut slice: Vec<i128> = Vec::new();
             slice.clear();
 
-            let mut counter: i32 = 0;
+            let mut counter: i128 = 0;
             while counter < size_of_slice {
                 slice.push(digits[(i + counter) as usize]);
                 counter += 1;
